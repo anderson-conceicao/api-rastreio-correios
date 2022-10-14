c178592c89c128dd44df2eff0d63084daf8e0ed9
Anderson Silva da Conceição <61567832+anderson-conceicao@users.noreply.github.com>
2022-10-13 22:19:04 -0300

    primeiro commit

---
+++ src/main/java/com/dev/apirastreiocorreios/ApiRastreioCorreiosApplication.java
@@ -0,0 +1,13 @@
+package com.dev.apirastreiocorreios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRastreioCorreiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRastreioCorreiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/apirastreiocorreios/ApiRastreioCorreiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.apirastreiocorreios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRastreioCorreiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
